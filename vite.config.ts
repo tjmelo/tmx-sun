348cc011b4de39c9b062e2652437d857f78b28e6
Thiago Melo <thiago.tmwd@gmail.com>
2025-01-13 16:09:33 -0300

    feat: conditional deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,31 +5,35 @@ import { federation } from '@module-federation/vite';
 import packageJson from './package.json'
 const deps = packageJson.dependencies
 
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: 'TmxHost ',
-      remotes: {
-        TmxMercury: 'TmxMercury@https://tjmelo.github.io/tmx-mercury/remoteEntry.js',
-      },
-      shared: {
-        react: {
-          requiredVersion: deps.react
-        },
-        'react-dom': {
-          requiredVersion: deps['react-dom']
-        },
-      },
-    }),
-  ],
-  server: {
-    watch: {
-      usePolling: true
-    }
-  },
-  build: {
-    target: 'chrome89',
-  },
-})
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production'; // Check if in production mode
 
+  return {
+    base: isProduction ? '/https://tjmelo.github.io/tmx-sun/' : '/', // Conditional base
+    plugins: [
+      react(),
+      federation({
+        name: 'TmxHost ',
+        remotes: {
+          TmxMercury: 'TmxMercury@https://tjmelo.github.io/tmx-mercury/remoteEntry.js',
+        },
+        shared: {
+          react: {
+            requiredVersion: deps.react
+          },
+          'react-dom': {
+            requiredVersion: deps['react-dom']
+          },
+        },
+      }),
+    ],
+    server: {
+      watch: {
+        usePolling: true
+      }
+    },
+    build: {
+      target: 'chrome89',
+    },
+  };
+});
